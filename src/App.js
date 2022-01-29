7e4aa084eb2702be7b60e28afe6f31f6c68cae06
Hilana <hilana.sene@gmail.com>
2022-01-29 19:17:48 +0100

    iterations 3 and 4

--- src/App.js
+++ src/App.js
@@ -4,24 +4,39 @@ import Navbar from './components/Navbar';
 import CountriesList from './components/CountriesList';
 import CountriesDetails from './components/CountriesDetails';
 import { Routes, Route } from 'react-router-dom';
-import { Link } from 'react-router-dom';
-import countries from './countries.json';
+import { useEffect, useState } from 'react';
+import { Link } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(' https://ih-countries-api.herokuapp.com/countries')
+      .then((response) => {
+        setCountries(response.data);
+      })
+      .catch((e) => console.log(e));
+  }, []);
   return (
     <div className="App">
       <Navbar />
-
       <div className="container">
         <div className="row">
-          <CountriesList countries={countries} />
-          <Routes>
-            <Route path="countries" />
-            <Route
-              path=":id"
-              element={<CountriesDetails countries={countries} />}
-            />
-          </Routes>
+          {countries.length === 0 ? (
+            <div className="loading"> Loading...</div>
+          ) : (
+            <>
+              <CountriesList countries={countries} />
+              <Routes>
+                <Route
+                  path="/:id"
+                  element={<CountriesDetails countries={countries} />}
+                />
+              </Routes>
+            </>
+          )}
         </div>
       </div>
     </div>
--- src/components/CountriesDetails.jsx
+++ src/components/CountriesDetails.jsx
@@ -1,33 +1,52 @@
+import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 import { useParams, Link } from 'react-router-dom';
 
 const CountriesDetails = ({ countries }) => {
   const [country, setCountry] = useState({});
+  const [borders, setBorders] = useState([]);
   const params = useParams();
-  const oneCountry = countries.find(
-    (country) => country.alpha3Code === params.id
-  );
-  //   console.log('one country:', oneCountry);
-  //   const currentCountry = oneCountry[0];
 
+  useEffect(() => {
+    axios
+      .get('https://ih-countries-api.herokuapp.com/countries/' + params.id)
+      .then(({ data }) => {
+        setCountry(data);
+      })
+      .catch((e) => console.log(e));
+  });
+
+  useEffect(() => {
+    if (!country.borders) return;
+    const bordersToAdd = country.borders.map((countryCode) => {
+      const foundCountry = countries.find(
+        (country) => country.alpha3Code === countryCode
+      );
+      return foundCountry.name.common;
+    });
+    setBorders(bordersToAdd);
+  }, [country]);
+
+  if (!country.name) return <div>Loading</div>;
   return (
     <div className="col-7">
       <img
         style={{ width: '40%' }}
-        src={`https://flagpedia.net/data/flags/icon/72x54/${oneCountry.alpha2Code.toLowerCase()}.png`}
+        src={`https://flagpedia.net/data/flags/icon/72x54/${country.alpha2Code.toLowerCase()}.png`}
+        alt={country.name}
       />
-      <h1>{oneCountry.name.common}</h1>
+      <h1>{country.name.common}</h1>
       <table className="table">
         <thead></thead>
         <tbody>
           <tr>
             <td style={{ width: '30%' }}>Capital</td>
-            <td>{oneCountry.capital[0]}</td>
+            <td>{country.capital[0]}</td>
           </tr>
           <tr>
             <td>Area</td>
             <td>
-              {oneCountry.area} km
+              {country.area} km
               <sup>2</sup>
             </td>
           </tr>
@@ -35,18 +54,18 @@ const CountriesDetails = ({ countries }) => {
             <td>Borders</td>
             <td>
               <ul>
-                {oneCountry.borders.map((border) => {
-                  const borderCountry = countries.filter(
-                    (country) => country.alpha3Code === border
-                  );
-                  return (
-                    <li key={borderCountry[0].alpha3Code}>
-                      <Link to={`/countries/${borderCountry[0].alpha3Code}`}>
-                        {borderCountry[0].flag} {borderCountry[0].name.common}
-                      </Link>{' '}
-                    </li>
-                  );
-                })}
+                {borders.length === 0 ? (
+                  <p>{country.name.common} has no bordering countries</p>
+                ) : (
+                  borders.map((c, i) => {
+                    console.log(country.borders);
+                    return (
+                      <li key={c}>
+                        <Link to={`/${country.borders[i]}`}>{c}</Link>
+                      </li>
+                    );
+                  })
+                )}
               </ul>
             </td>
           </tr>
